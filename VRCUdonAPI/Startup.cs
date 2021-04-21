ea18c42591898778118b1054f1de0c4939046283
Jón Gunnar <jgh@tskoli.is>
2021-04-21 18:40:37 +0000

    Added mappings for error -> errordto and vice-versa

--- VRCUdonAPI/Startup.cs
+++ VRCUdonAPI/Startup.cs
@@ -77,10 +77,10 @@ namespace VRCUdonAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ExceptionMiddleware>();
 
             if (env.IsDevelopment())
             {
-                app.UseMiddleware<ExceptionMiddleware>();
                 //app.UseDeveloperExceptionPage();
             }
             else
